7727f8747d6c9844430e2e4722424ce58005b20b
Dharmendra Vaishnav <dharmendra.vaishnav@metacube.com>
2020-09-03 14:37:13 +0530

    Added atlantis

--- main.tf
+++ main.tf
@@ -4,21 +4,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.21.0"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "2.21.0"
 
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+#   name = var.vpc_name
+#   cidr = var.vpc_cidr
 
-  azs             = var.vpc_azs
-  private_subnets = var.vpc_private_subnets
-  public_subnets  = var.vpc_public_subnets
+#   azs             = var.vpc_azs
+#   private_subnets = var.vpc_private_subnets
+#   public_subnets  = var.vpc_public_subnets
 
-  enable_nat_gateway = var.vpc_enable_nat_gateway
+#   enable_nat_gateway = var.vpc_enable_nat_gateway
 
-  tags = var.vpc_tags
-}
+#   tags = var.vpc_tags
+# }
 
 # module "ec2_instances" {
 #   source  = "terraform-aws-modules/ec2-instance/aws"
